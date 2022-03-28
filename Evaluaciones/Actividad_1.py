aa416f484d0d5c1e7714368245ff46fd29305364
kumash01 <100644036+kumash01@users.noreply.github.com>
2022-03-28 17:40:33 -0400

    Update Actividad_1.py

--- Evaluaciones/Actividad_1.py
+++ Evaluaciones/Actividad_1.py
@@ -1,5 +1,5 @@
 #Implemente una tupla de un solo elemento.
 
-uniTupla=('Python')
+uniTupla=('Python',)
 
-print("Estoy aprendiendo a programar en:",uniTupla[:])
+print("Estoy aprendiendo a programar en:",uniTupla[0])
